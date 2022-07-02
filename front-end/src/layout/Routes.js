2f0e3ac05a0e447915b4bb4bbde174fda9459554
Molly <mojonesycreative@gmail.com>
2022-07-02 17:52:02 -0400

    added CreateReservation component/route

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
+import CreateReservation from "../reservations/CreateReservation";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
 
@@ -18,12 +19,19 @@ function Routes() {
       <Route exact={true} path="/">
         <Redirect to={"/dashboard"} />
       </Route>
+
       <Route exact={true} path="/reservations">
         <Redirect to={"/dashboard"} />
       </Route>
+
       <Route path="/dashboard">
         <Dashboard date={today()} />
       </Route>
+
+      <Route path="/reservations/new">
+        <CreateReservation />
+      </Route>
+
       <Route>
         <NotFound />
       </Route>
