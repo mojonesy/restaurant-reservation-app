9956d743f74190bbf0a7df098a223630ebd92936
Molly <mojonesycreative@gmail.com>
2022-07-04 11:46:30 -0400

    created methodNotAllowed handler

---
+++ back-end/src/errors/methodNotAllowed.js
@@ -0,0 +1,12 @@
+/*
+ * Express API "Method Not Allowed" handler
+ */
+
+function methodNotAllowed(req, res, next) {
+    next({
+      status: 405,
+      message: `${req.method} not allowed for ${req.orginalUrl}`,
+    });
+}
+
+module.exports = methodNotAllowed;
