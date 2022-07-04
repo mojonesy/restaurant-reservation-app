44d7effd2ecbde694b06190c9b10a4b693f38d5a
Molly <mojonesycreative@gmail.com>
2022-07-04 14:04:22 -0400

    list reservation by date query params

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -5,7 +5,8 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
  * List handler for reservation resources
  */
 async function list(req, res) {
-  const data = await service.list();
+  const date = req.query.date;
+  const data = await service.list(date);
   res.json({ data });
 }
 
