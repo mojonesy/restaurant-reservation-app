38bda76e1c6496a4e0643066869150cfe9276dd7
Molly <mojonesycreative@gmail.com>
2022-07-04 11:39:28 -0400

    created asyncErrorBoundary

---
+++ back-end/src/errors/asyncErrorBoundary.js
@@ -0,0 +1,13 @@
+export default function asyncErrorBoundary(delegate, defaultStatus) {
+  return (request, response, next) => {
+    Promise.resolve()
+      .then(() => delegate(request, response, next))
+      .catch((error) => {
+        const { status = defaultStatus, message = error } = error || {};
+        next({
+          status,
+          message,
+        });
+      });
+  };
+}
