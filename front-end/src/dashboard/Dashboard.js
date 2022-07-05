e1b2cd02ddd906f9a9338bc8dbc36ce709344874
Molly <mojonesycreative@gmail.com>
2022-07-05 12:43:13 -0400

    added reservation cards & date nav buttons

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { listReservations } from "../utils/api";
+import ReservationCard from "./ReservationCard";
+import DateNavButtons from "./DateNavButtons";
 import ErrorAlert from "../layout/ErrorAlert";
 
 /**
@@ -9,6 +11,7 @@ import ErrorAlert from "../layout/ErrorAlert";
  * @returns {JSX.Element}
  */
 function Dashboard({ date }) {
+
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
 
@@ -23,14 +26,45 @@ function Dashboard({ date }) {
     return () => abortController.abort();
   }
 
+  /* If no error is returned from server, display info */
+  if (reservationsError === null) {
+    return (
+      <main>
+        <h1>Dashboard</h1>
+        <div className="d-md-flex mb-3">
+          <h4 className="mb-0">Reservations for {date}</h4>
+        </div>
+        
+        <div id="reservationGrid" className="row row-cols-1">
+          {reservations.map((reservation) => (
+            <div className="col">
+              <ReservationCard
+                reservation_id={reservation.reservation_id}
+                first_name={reservation.first_name}
+                last_name={reservation.last_name}
+                mobile_number={reservation.mobile_number}
+                reservation_date={reservation.reservation_date}
+                reservation_time={reservation.reservation_time}
+                people={reservation.people}
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className="dateNav">
+          <DateNavButtons currentDate={date} />
+        </div>
+      </main>
+    );
+  }
+
   return (
     <main>
       <h1>Dashboard</h1>
       <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
+        <h4 className="mb-0">Reservations for {date}</h4>
       </div>
       <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
     </main>
   );
 }
