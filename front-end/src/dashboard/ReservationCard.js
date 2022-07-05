9e69a45491710cdaac49549f5456fd41ae7d749a
Molly <mojonesycreative@gmail.com>
2022-07-05 12:42:56 -0400

    view reservation card component

---
+++ front-end/src/dashboard/ReservationCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function ReservationCard(
+  { reservation_id, 
+    first_name, 
+    last_name, 
+    mobile_number, 
+    reservation_date, 
+    reservation_time, 
+    people }
+) {
+
+    
+  return (
+    <div class="card border-secondary mb-3" key={reservation_id} style={{maxWidth: "18rem"}}>
+      <h4 className="card-header">{last_name}, {first_name}</h4>
+      <div className="card-body">
+        <h5 className="card-title">{reservation_time}, {reservation_date}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">Guests: {people}</h6>
+        <h6 className="card-subtitle mb-2 text-muted">Mobile Number: {mobile_number}</h6>
+      </div>
+      <div 
+        className="card-footer border-secondary text-secondary"
+        style={{textAlign: "right"}} >
+        Reservation #: {reservation_id}
+      </div>
+    </div>
+  );
+}
+
+export default ReservationCard;
