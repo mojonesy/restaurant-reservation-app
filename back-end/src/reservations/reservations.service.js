b321655b8d646808011fb70304fd590a5306e88f
Molly <mojonesycreative@gmail.com>
2022-07-05 13:18:20 -0400

    insert new reservation

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -7,6 +7,14 @@ function list(date) {
     .orderBy("reservation_time");
 }
 
+function create(newReservation) {
+  return knex("reservations")
+    .insert(newReservation)
+    .returning("*")
+    .then(createdRecords => createdRecords[0]);
+}
+
 module.exports = {
+    create,
     list,
 }
