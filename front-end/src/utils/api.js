47229d4dda9f70164e0e6ff12dc2cda17419232c
Molly <mojonesycreative@gmail.com>
2022-07-05 12:50:19 -0400

    remove "next" import

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -2,7 +2,6 @@
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-import { next } from "./date-time";
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
