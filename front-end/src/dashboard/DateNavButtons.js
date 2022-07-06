6d9d57120d94eddc0783036e6e4263eb47c95e24
Molly <mojonesycreative@gmail.com>
2022-07-06 11:40:53 -0400

    removed console.log before deployment

--- front-end/src/dashboard/DateNavButtons.js
+++ front-end/src/dashboard/DateNavButtons.js
@@ -12,7 +12,6 @@ function DateNavButtons({ currentDate }) {
   }
   const handleToday = (event) => {
     event.preventDefault();
-    console.log(currentDate);
     history.push(`/dashboard?date=${today()}`);
   }
   const handleNext = (event) => {
