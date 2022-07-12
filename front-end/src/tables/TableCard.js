927f48d8e35deebfa4d6a77b733ef457c945484b
Molly <mojonesycreative@gmail.com>
2022-07-11 21:00:57 -0400

    initial TableCard component

---
+++ front-end/src/tables/TableCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function TableCard() {
+  return (
+    <div className="card border-secondary mb-3" style={{maxWidth: "25rem"}}>
+    </div>
+  );
+}
+
+export default TableCard;
