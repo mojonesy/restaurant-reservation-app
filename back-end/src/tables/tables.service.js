a864b3f2684e356b6890f551c176ab088b627475
Molly <mojonesycreative@gmail.com>
2022-07-13 12:57:11 -0400

    services for read, readReservation, update

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -13,7 +13,33 @@ function create(newTable) {
     .then(createdRecords => createdRecords[0]);
 }
 
+function read(table_id) {
+  return knex("tables")
+    .select("*")
+    .where({"table_id": table_id})
+    .first();
+}
+
+// Read reservation by id in order to update a table //
+function readReservation(reservation_id) {
+  return knex("reservations")
+    .select("*")
+    .where({"reservation_id": reservation_id})
+    .first();
+}
+
+function update(updatedTable) {
+  return knex("tables")
+    .select("*")
+    .where({"table_id": updatedTable.table_id})
+    .update(updatedTable, "*")
+    .then(updatedRecords => updatedRecords[0]);
+}
+
 module.exports = {
     create,
     list,
+    read,
+    readReservation,
+    update,
 }
