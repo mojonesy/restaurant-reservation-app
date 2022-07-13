3739c4704bf1e9bdc7cff194ad8236666981e895
Molly <mojonesycreative@gmail.com>
2022-07-12 22:37:02 -0400

    added post request

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -4,6 +4,7 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.route("/")
   .get(controller.list)
+  .post(controller.create)
   .all(methodNotAllowed);
 
 module.exports = router;
