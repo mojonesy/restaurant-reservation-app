cdc99644c3310896dfccede5b2e3a2eef6989c3c
Molly <mojonesycreative@gmail.com>
2022-07-12 22:36:09 -0400

    adjusted reservation_id to nullable

--- back-end/src/db/migrations/20220710203411_createTablesTable.js
+++ back-end/src/db/migrations/20220710203411_createTablesTable.js
@@ -3,7 +3,7 @@ exports.up = function (knex) {
     table.increments("table_id").primary();
     table.string("table_name").notNullable();
     table.integer("capacity").unsigned().notNullable();
-    table.integer("reservation_id").unsigned().notNullable();
+    table.integer("reservation_id").unsigned();
     table
       .foreign("reservation_id")
       .references("reservation_id")
