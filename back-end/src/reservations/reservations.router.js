d65a39b09608e37316c748b0ca187883758bf658
Molly <mojonesycreative@gmail.com>
2022-07-13 11:57:16 -0400

    route for /reservations/:reservation_id

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -2,6 +2,10 @@ const router = require("express").Router();
 const controller = require("./reservations.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
+router.route("/:reservation_id")
+  .get(controller.read)
+  .all(methodNotAllowed);
+
 router.route("/")
   .get(controller.list)
   .post(controller.create)
