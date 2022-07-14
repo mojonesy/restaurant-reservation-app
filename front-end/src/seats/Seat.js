d08be67681a79ebaa09183653e4fbf6971fe6ef0
Molly <mojonesycreative@gmail.com>
2022-07-13 23:23:52 -0400

    seat form implemented

--- front-end/src/seats/Seat.js
+++ front-end/src/seats/Seat.js
@@ -1,13 +1,98 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { listTables, readReservation, updateTable } from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert";
 
 function Seat() {
   const reservation_id = useParams().reservation_id;
+  const history = useHistory();
+
+  const [error, setError] = useState(null);
+  const [tables, setTables] = useState([]);
+  const [reservation, setReservation] = useState([]);
+  const [selectedTable, setSelectedTable] = useState({
+    reservation_id: reservation_id,
+  });
+
+  // Load tables //
+  useEffect(() => {
+    async function loadTables() {
+      const response = await listTables();
+      setTables(response);
+    }
+    loadTables();
+  }, []);
+  
+  // Load reservation //
+  useEffect(() => {
+    async function loadReservation() {
+      const response = await readReservation(reservation_id);
+      setReservation(response);
+    }
+    loadReservation();
+  }, [reservation_id]);
+
+  // Handle table selection //
+  const handleChange = ({ target }) => {
+    setSelectedTable({ ...selectedTable, [target.name]: target.value });
+    console.log(target.value);
+  }
+
+  // Submit and send PUT request to update table //
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    updateTable(reservation_id, selectedTable.table_id, selectedTable)
+      .then(() => history.push("/dashboard"))
+      .catch((error) => setError(error));
+  }
+
 
   return (
-    <>
-      <h1>Seat</h1>
-    </>
+    <main>
+      <div className="d-md-flex mb-3">
+        <h1>Seating</h1>
+      </div>
+
+      <ErrorAlert error={error} setError={setError} />
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="table-select">
+          Assign a table for reservation #{reservation_id}, for {reservation.people} people:
+        </label>
+        <div className="selections">
+          <select 
+            name="table_id" 
+            id="table-select"
+            onChange={handleChange}
+          >
+            <option value="">- Please choose a table -</option>
+            {tables.map((table) => (
+              <option value={table.table_id} key={table.table_name}>
+                {table.table_name} - {table.capacity}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="form-buttons" style={{marginTop: "10px"}}>
+          <button 
+            type="submit"
+            className="btn btn-primary btn-lg"
+            style={{marginRight: "10px"}}
+          >
+            Submit
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary btn-lg"
+            onClick={() => history.go(-1)}
+          >
+            Cancel
+          </button>
+        </div>
+
+      </form>
+    </main>
   );
 }
 
