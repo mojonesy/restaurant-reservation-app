8ee946f9ea9de18996979277486218bc60a7eacc
Molly <mojonesycreative@gmail.com>
2022-07-14 12:16:20 -0400

    removed console.log

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -143,7 +143,6 @@ function isWithinBusinessHours(req, res, next) {
  */
 async function create(req, res) {
   const data = await service.create(req.body.data);
-  console.log("data", data);
   res.status(201).json({ data: data });
 }
 
