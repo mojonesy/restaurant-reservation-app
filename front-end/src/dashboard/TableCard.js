3f0ff0f1e6ca94201ea760658872c06b93eb7415
Molly <mojonesycreative@gmail.com>
2022-07-15 20:49:46 -0400

    window.confirm when clicking "Finish"

--- front-end/src/dashboard/TableCard.js
+++ front-end/src/dashboard/TableCard.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { removeReservation, listTables } from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert";
 import "./TableCard.css";
 
 function TableCard({
@@ -8,10 +10,22 @@ function TableCard({
   reservation_id
 }) {
 
+  const [error, setError] = useState(null);
+
+  const handleFinish = () => {
+    if (window.confirm("Is this table ready to seat new guests? This cannot be undone.")) {
+      removeReservation(table_id)
+        .then(() => listTables())
+        .catch((error) => setError(error));
+    }
+  }
+
 
   return (
+    <>
     <div className="card">
       <div className="card-body">
+        <ErrorAlert error={error} setError={setError} />
         <span className="badge bg-info">{capacity}</span>
         <h6 className="card-title">{table_name}</h6>
         <p className="card-subtitle mb-2 text-muted">Reservation #{reservation_id}</p>
@@ -25,6 +39,7 @@ function TableCard({
             <button 
               type="button" 
               className="btn btn-dark"
+              onClick={handleFinish}
               data-table-id-finish={table_id}
               style={{marginLeft: "5px"}}
               >
@@ -32,8 +47,26 @@ function TableCard({
               </button>
             }
         </div>
+        {/* <div className="modal fade" id="finishSeatModal" tabIndex={-1} aria-labelledby="finishSeatModal" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-center">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="finishSeatModal">Title</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                Body
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" className="btn btn-primary">Ok</button>
+              </div>
+            </div>
+          </div>
+        </div> */}
       </div>
     </div>
+    </>
   );
 }
 
