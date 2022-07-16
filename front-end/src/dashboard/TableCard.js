70744dac8309c78a617f9b59552bdeb5673d40d3
Molly <mojonesycreative@gmail.com>
2022-07-16 18:53:15 -0400

    handleFinish removes res and refreshes page - can't figure out modal

--- front-end/src/dashboard/TableCard.js
+++ front-end/src/dashboard/TableCard.js
@@ -1,22 +1,23 @@
-import React, { useState } from "react";
-import { removeReservation, listTables } from "../utils/api";
-import ErrorAlert from "../layout/ErrorAlert";
+import React from "react";
+import { removeReservation } from "../utils/api";
 import "./TableCard.css";
 
 function TableCard({
   table_id,
   table_name,
   capacity,
-  reservation_id
+  reservation_id,
+  setTablesError
 }) {
 
-  const [error, setError] = useState(null);
-
-  const handleFinish = () => {
-    if (window.confirm("Is this table ready to seat new guests? This cannot be undone.")) {
-        removeReservation(table_id)
-          .then(window.location.reload());
-      }
+  const handleFinish = (event) => {
+    event.preventDefault();
+    const message = "Is this table ready to seat new guests? This cannot be undone.";
+    if (window.confirm(message)) {
+      removeReservation(table_id)
+        .then(window.location.reload())
+        .catch(setTablesError);
+    }
   }
 
 
@@ -24,7 +25,6 @@ function TableCard({
     <>
     <div className="card">
       <div className="card-body">
-        <ErrorAlert error={error} setError={setError} />
         <span className="badge bg-info">{capacity}</span>
         <h6 className="card-title">{table_name}</h6>
         <p className="card-subtitle mb-2 text-muted">Reservation #{reservation_id}</p>
@@ -46,23 +46,6 @@ function TableCard({
               </button>
             }
         </div>
-        {/* <div className="modal fade" id="finishSeatModal" tabIndex={-1} aria-labelledby="finishSeatModal" aria-hidden="true">
-          <div className="modal-dialog modal-dialog-center">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="finishSeatModal">Title</h5>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div className="modal-body">
-                Body
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                <button type="button" className="btn btn-primary">Ok</button>
-              </div>
-            </div>
-          </div>
-        </div> */}
       </div>
     </div>
     </>
@@ -70,3 +53,26 @@ function TableCard({
 }
 
 export default TableCard;
+
+
+
+/* This isn't working, but want to try again later */
+
+  // {/* Modal */}
+  // {reservation_id &&
+  //   <div className="modal fade" id="finishSeatModal" tabIndex={-1} aria-labelledby="finishSeatModal" aria-hidden="true">
+  //     <div className="modal-dialog modal-dialog-centered">
+  //       <div className="modal-content">
+  //         <div className="modal-header">
+  //           <h5 className="modal-title" id="finishSeatModal">Is this table ready to seat new guests?</h5>
+  //         </div>
+  //         <div className="modal-body">
+  //           This cannot be undone.
+  //         </div>
+  //         <div className="modal-footer">
+  //           <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+  //           <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={handleOk}>Ok</button>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>}
