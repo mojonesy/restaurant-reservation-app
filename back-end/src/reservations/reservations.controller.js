5cf2093d21b8a1c90b699186da33b0294cb7455b
Molly <mojonesycreative@gmail.com>
2022-07-18 12:57:47 -0400

    added status validations

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -139,8 +139,39 @@ function isWithinBusinessHours(req, res, next) {
   }
 }
 
+function hasDefaultBookedStatus(req, res, next) {
+  const { status } = req.body.data;
+  if (status && status !== "booked") {
+    next({ status: 400, message: "a new reservation must have a default status of 'booked.'" });
+  } else {
+    next();
+  }
+}
+
+ function hasValidStatus(req, res, next) {
+  const validStatuses = ["booked", "seated", "finished"];
+  const { status } = req.body.data;
+  if (status && !validStatuses.includes(status)) {
+    next({ 
+      status: 400, 
+      message: `Invalid status: '${status}.' Status must be either 'booked', 'seated', or 'finished.' `
+    });
+  } else {
+    next();
+  }
+ }
+
+ function isFinished(req, res, next) {
+  const currentStatus = res.locals.reservation.status;
+  if (currentStatus === "finished") {
+    next({ status: 400, message: "A finished reservation cannot be updated." });
+  } else {
+    next();
+  }
+ }
+
 /**
- * Create new reservation handler
+ * Create a new reservation
  */
 async function create(req, res) {
   const data = await service.create(req.body.data);
@@ -163,8 +194,20 @@ async function reservationExists(req, res, next) {
 /**
  * Read given reservation by ID
  */
-function read(req, res, next) {
+function read(req, res) {
   const data = res.locals.reservation;
+  res.json({ data });
+}
+
+/**
+ * Update reservation by ID
+ */
+async function update(req, res) {
+  const updatedReservation = {
+    ...req.body.data,
+    reservation_id: res.locals.reservation.reservation_id,
+  };
+  const data = await service.update(updatedReservation);
   res.json({ data });
 }
 
@@ -180,11 +223,19 @@ module.exports = {
     IsNotTuesday,
     IsNotPastDate,
     isWithinBusinessHours,
+    hasDefaultBookedStatus,
     asyncErrorBoundary(create),
   ],
   list: asyncErrorBoundary(list),
   read: [
     asyncErrorBoundary(reservationExists),
     read,
+  ],
+  update: [
+    hasData,
+    asyncErrorBoundary(reservationExists),
+    hasValidStatus,
+    isFinished,
+    asyncErrorBoundary(update),
   ],
 };
