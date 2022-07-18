44d19d81b10158d5ddd84da7f0e0bab41a687d45
Molly <mojonesycreative@gmail.com>
2022-07-18 12:58:39 -0400

    updated route from '/:res_id/seat' to '/:res_id/status'

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const controller = require("./reservations.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/:reservation_id/seat")
+router.route("/:reservation_id/status")
   .put(controller.update)
   .all(methodNotAllowed);
 
