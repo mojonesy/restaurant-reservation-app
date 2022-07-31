e699a104c1424f0ffa4c46c8b7f84b7420d9de15
Molly <mojonesycreative@gmail.com>
2022-07-31 16:41:58 -0400

    import ErrorAlert, useHistory/Params, load/set res by id

--- front-end/src/reservations/EditReservation.js
+++ front-end/src/reservations/EditReservation.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { readReservation } from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert";
 
 function EditReservation() {
+  const history = useHistory();
+  const reservation_id = useParams().reservation_id;
+
+  const [error, setError] = useState(null);
+  const [reservation, setReservation] = useState("");
+
+  // Load reservation by id //
+  useEffect(() => {
+    async function loadReservation() {
+      const response = await readReservation(reservation_id);
+      setReservation(response);
+    }
+    loadReservation();
+  }, [reservation_id]);
+
   return (
     <main>
-      <h1>Edit Reservation</h1>
+      <div className="d-md-flex mb-3">
+        <h1>Edit Reservation</h1>
+        <ErrorAlert error={error} setError={setError} />
+      </div>
     </main>
   );
 }
