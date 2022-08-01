a44b2c9d875a4cb503ecdf8fefb83a34707d9c55
Molly <mojonesycreative@gmail.com>
2022-08-01 16:59:32 -0400

    removed 'reservations' dependency from useEffect - was causing issues

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -21,7 +21,7 @@ function Dashboard({ date }) {
   useEffect(() => {
     loadReservationsAndTables();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [date, reservations]);
+  }, [date]);
 
   function loadReservations() {
     const abortController = new AbortController();
