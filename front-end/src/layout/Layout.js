8c6927ffd8cb8044382360a5370a6a8d641728f9
Molly <mojonesycreative@gmail.com>
2022-08-01 23:29:04 -0400

    added 'main-body' class

--- front-end/src/layout/Layout.js
+++ front-end/src/layout/Layout.js
@@ -18,7 +18,7 @@ function Layout() {
         <div className="col-md-2 side-bar">
           <Menu />
         </div>
-        <div className="col">
+        <div className="col main-body">
           <Routes />
         </div>
       </div>
