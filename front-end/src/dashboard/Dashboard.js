ac5be1e3bd14be7979d8f1a971a1e0aeda2e2d06
Molly <mojonesycreative@gmail.com>
2022-08-03 12:10:08 -0400

    add css file

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import ReservationsList from "../reservations/ReservationsList";
 import TableCard from "./TableCard";
 import DateNavButtons from "./DateNavButtons";
 import ErrorAlert from "../layout/ErrorAlert";
+import "./Dashboard.css";
 
 /**
  * Defines the dashboard page.
@@ -52,14 +53,14 @@ function Dashboard({ date }) {
     return (
       <main>
         <h1>Dashboard</h1>
-        <div className="d-md-flex flex-column mb-3">
+        <div className="d-md-flex flex-column">
           {!reservations.length && <h2>No reservations on this date.</h2>}
-          <h4 className="mb-0">Reservations for {date}</h4>
         </div>
         <ErrorAlert error={reservationsError} setError={setReservationsError} />
 
         {/* Reservations */}
-        <div className="reservationsList">
+        <div className="reservations-list">
+          <h4 className="mb-2">Reservations for {date}</h4>
           <ReservationsList 
             reservations={reservations}
             setReservationsError={setReservationsError}
@@ -72,23 +73,25 @@ function Dashboard({ date }) {
         </div>
 
         {/* Tables */}
-        <div className="d-md-flex mb-3">
-          <h4 className="mb-0">Tables</h4>
-        </div>
-        <ErrorAlert error={tablesError} setError={setTablesError} />
-        <div id="tableGrid" className="row row-cols-4">
-          {tables.map((table) => (
-            <div className="col-sm-3" key={table.table_id}>
-              <TableCard
-                table_id={table.table_id}
-                table_name={table.table_name}
-                capacity={table.capacity}
-                reservation_id={table.reservation_id}
-                setTablesError={setTablesError}
-                loadReservationsAndTables={loadReservationsAndTables}
-              />
-            </div>
-          ))}
+        <div className="tables-list">
+          <div className="d-md-flex mb-3">
+            <h4 className="mb-0">Tables</h4>
+          </div>
+          <ErrorAlert error={tablesError} setError={setTablesError} />
+          <div id="tableGrid" className="row row-cols-4">
+            {tables.map((table) => (
+              <div className="col-sm-3" key={table.table_id}>
+                <TableCard
+                  table_id={table.table_id}
+                  table_name={table.table_name}
+                  capacity={table.capacity}
+                  reservation_id={table.reservation_id}
+                  setTablesError={setTablesError}
+                  loadReservationsAndTables={loadReservationsAndTables}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </main>
     );
