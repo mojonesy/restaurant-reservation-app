c9d0cc5e121c1c10e63f7455b50ec68e6dfec963
Molly <mojonesycreative@gmail.com>
2022-08-04 17:27:17 -0400

    import css file

--- front-end/src/reservations/CreateReservation.js
+++ front-end/src/reservations/CreateReservation.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { createReservation } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
+import "./CreateReservation.css";
 
 function CreateReservation() {
   const history = useHistory();
