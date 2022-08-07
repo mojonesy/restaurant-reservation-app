0ec0a646b89bcf59e786e3dd695e31a0ac32a265
Molly <mojonesycreative@gmail.com>
2022-08-06 23:21:04 -0400

    removed modals

--- front-end/src/reservations/CancelReservationButton.js
+++ front-end/src/reservations/CancelReservationButton.js
@@ -1,22 +1,14 @@
-import React, { useState } from "react";
-import { Button, Modal } from "react-bootstrap";
+import React from "react";
 import { cancelReservation } from "../utils/api";
 
 function CancelReservationButton({ reservation_id, setReservationsError, loadReservationsAndTables }) {
-  
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   const handleOk = (event) => {
     event.preventDefault();
-    // Want to use modal - come back to this later. //
     const message = "Do you want to cancel this reservation? This cannot be undone.";
     if (window.confirm(message)) {
       cancelReservation(reservation_id, "cancelled")
         .then(() => loadReservationsAndTables())
-        // .then(handleClose)
         .catch(setReservationsError);
     }
   };
@@ -27,27 +19,10 @@ function CancelReservationButton({ reservation_id, setReservationsError, loadRes
       <button
         type="button"
         className="btn btn-danger"
-        // Use handleShow here for modal
         onClick={handleOk}
         data-reservation-id-cancel={reservation_id}>
           Cancel
       </button>
-
-      {/* Modal */}
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header>
-          <Modal.Title>Do you want to cancel this reservation?</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>This cannot be undone.</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Cancel
-          </Button>
-          <Button variant="primary" onClick={handleOk}>
-            Ok
-          </Button>
-        </Modal.Footer>
-      </Modal>
     </>
   );
 }
--- front-end/src/tables/TableCard.js
+++ front-end/src/tables/TableCard.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { Button, Modal } from "react-bootstrap";
+import React from "react";
 import { removeReservation } from "../utils/api";
 import "./TableCard.css";
 import { Icon } from '@iconify/react';
@@ -13,18 +12,12 @@ function TableCard({
   loadReservationsAndTables,
 }) {
 
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   const handleFinish = (event) => {
     event.preventDefault();
     const message = "Is this table ready to seat new guests? This cannot be undone.";
     if (window.confirm(message)) {
       removeReservation(table_id)
         .then(() => loadReservationsAndTables())
-        .then(handleClose)
         .catch(setTablesError);
     }
   }
@@ -32,52 +25,34 @@ function TableCard({
 
   return (
     <>
-    <div className="card">
-      <div className="card-body">
-        <span className="badge capacity-badge">
-          <Icon className="people-icon" icon="bi:people" color="#f8f8f4" />
-          {capacity}
-        </span>
+      <div className="card">
+        <div className="card-body">
+          <span className="badge capacity-badge">
+            <Icon className="people-icon" icon="bi:people" color="#f8f8f4" />
+            {capacity}
+          </span>
 
-        <h6 className="card-title">{table_name}</h6>
-        <p className="card-subtitle mb-2 text-muted">Reservation {reservation_id}</p>
-        <div 
-          className={`alert ${reservation_id ? "alert-warning" : "alert-success"}`} 
-          id="statusWithFinishButton"
-          role="alert" 
-          data-table-id-status={table_id}>
-            {reservation_id ? "Occupied" : "Free"}
-            {reservation_id && 
-              <button 
-                type="button" 
-                className="btn"
-                id="finishButton"
-                // Use handleShow here for modal
-                onClick={handleFinish}
-                data-table-id-finish={table_id}>
-                  Finish
-                </button>
-            }
+          <h6 className="card-title">{table_name}</h6>
+          <p className="card-subtitle mb-2 text-muted">Reservation {reservation_id}</p>
+          <div 
+            className={`alert ${reservation_id ? "alert-warning" : "alert-success"}`} 
+            id="statusWithFinishButton"
+            role="alert" 
+            data-table-id-status={table_id}>
+              {reservation_id ? "Occupied" : "Free"}
+              {reservation_id && 
+                <button 
+                  type="button" 
+                  className="btn"
+                  id="finishButton"
+                  onClick={handleFinish}
+                  data-table-id-finish={table_id}>
+                    Finish
+                  </button>
+              }
+          </div>
         </div>
       </div>
-    </div>
-
-   {/* Modal */}
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header>
-          <Modal.Title>Is this table ready to seat new guests?</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>This cannot be undone.</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Cancel
-          </Button>
-          <Button variant="primary" onClick={handleFinish}>
-            Ok
-          </Button>
-        </Modal.Footer>
-      </Modal>
-
     </>
   );
 }
