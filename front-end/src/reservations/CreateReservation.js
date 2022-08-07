56edbbe1219907a5c82ab8aa294a62dc4c5c254c
Molly <mojonesycreative@gmail.com>
2022-08-07 11:53:26 -0400

    passed data to ReservationForm component

--- front-end/src/reservations/CreateReservation.js
+++ front-end/src/reservations/CreateReservation.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { createReservation } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
+import ReservationForm from "./ReservationForm";
 import "./CreateReservation.css";
 
 function CreateReservation() {
@@ -36,130 +37,21 @@ function CreateReservation() {
     return () => abortController.abort();
   };
 
-  const handleReset = (event) => {
-    event.preventDefault();
-    setReservation({ ...initialFormState });
-  };
 
-
-    return (
-      <main>
-        <div className="d-md-flex mb-3">
-          <h1>Create a New Reservation</h1>
-          <ErrorAlert error={error} setError={setError}/>
-        </div>
-        
-        {/* Reservation Form */}
-        <form onSubmit={handleSubmit}>
-          <div className="row mb-3">
-            <div className="col">
-              <label htmlFor="first_name" className="form-label">First Name</label>
-              <input
-                type="text"
-                name="first_name"
-                id="first_name"
-                className="form-control"
-                onChange={handleChange}
-                value={reservation.first_name}
-                required
-              />
-            </div>
-            <div className="col">
-              <label htmlFor="last_name" className="form-label">Last Name</label>
-              <input
-                type="text"
-                name="last_name"
-                id="last_name"
-                className="form-control"
-                onChange={handleChange}
-                value={reservation.last_name}
-                required
-              />
-            </div>
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="mobile_number" className="form-label">Mobile Number</label>
-            <input
-              type="tel"
-              name="mobile_number"
-              id="mobile_number"
-              className="form-control"
-              onChange={handleChange}
-              value={reservation.mobile_number}
-              pattern="^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$"
-              required
-            />
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="date" className="form-label">Date</label>
-            <input 
-              type="date"
-              name="reservation_date" 
-              id="reservation_date"
-              className="form-control" 
-              onChange={handleChange}
-              value={reservation.reservation_date}
-              placeholder="YYYY-MM-DD" 
-              pattern="\d{4}-\d{2}-\d{2}"
-              required
-            />
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="time" className="form-label">Time</label>
-            <input 
-              type="time" 
-              name="reservation_time"
-              id="reservation_time"
-              className="form-control" 
-              onChange={handleChange}
-              value={reservation.reservation_time}
-              placeholder="HH:MM" 
-              pattern="[0-9]{2}:[0-9]{2}"
-              required
-            />
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="people" className="form-label">Number of Guests</label>
-            <input
-              type="number"
-              name="people"
-              id="people"
-              className="form-control"
-              onChange={handleChange}
-              value={reservation.people}
-              min="1"
-              max="8"
-              required
-            />
-          </div>
-
-          <button 
-            type="submit"
-            className="btn btn-primary btn-lg"
-            style={{marginRight: "10px"}}>
-              Submit
-          </button>
-          <button
-            type="reset"
-            className="btn btn-secondary btn-lg"
-            style={{marginRight: "10px"}}
-            onClick={handleReset}>
-              Reset Form
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary btn-lg"
-            onClick={() => history.go(-1)}>
-              Cancel
-          </button>
-
-        </form>
-      </main>
-    );
+  return (
+    <main>
+      <div className="d-md-flex mb-3">
+        <h1>Create a New Reservation</h1>
+        <ErrorAlert error={error} setError={setError}/>
+      </div>
+      
+      <ReservationForm
+        reservation={reservation}
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+      />
+    </main>
+  );
   }
 
 
