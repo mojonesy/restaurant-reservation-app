2abc889552ae4ba7735d61f5425ff8f75a65dcb8
Molly <mojonesycreative@gmail.com>
2022-08-06 23:15:32 -0400

    reformatting

--- front-end/src/reservations/ReservationsList.js
+++ front-end/src/reservations/ReservationsList.js
@@ -7,19 +7,19 @@ function ReservationsList({ reservations, setReservationsError, loadReservations
     <div id="reservationGrid" className="row row-cols-1">
       {reservations.map((reservation) => (
         <div className="reservation-card col-sm" key={reservation.reservation_id}>
-            <ReservationCard
-              reservation_id={reservation.reservation_id}
-              first_name={reservation.first_name}
-              last_name={reservation.last_name}
-              mobile_number={reservation.mobile_number}
-              reservation_date={reservation.reservation_date}
-              reservation_time={reservation.reservation_time.slice(0, 5)}
-              people={reservation.people}
-              status={reservation.status}
-              setReservationsError={setReservationsError}
-              loadReservationsAndTables={loadReservationsAndTables}
-            />
-          </div>
+          <ReservationCard
+            reservation_id={reservation.reservation_id}
+            first_name={reservation.first_name}
+            last_name={reservation.last_name}
+            mobile_number={reservation.mobile_number}
+            reservation_date={reservation.reservation_date}
+            reservation_time={reservation.reservation_time.slice(0, 5)}
+            people={reservation.people}
+            status={reservation.status}
+            setReservationsError={setReservationsError}
+            loadReservationsAndTables={loadReservationsAndTables}
+          />
+        </div>
       ))}
     </div>
   );
