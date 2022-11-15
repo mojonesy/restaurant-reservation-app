c5397b6974e42accb72909ff4e8136aa59787f99
Molly <mojonesycreative@gmail.com>
2022-11-15 18:11:43 -0500

    set new api_base_url

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -7,7 +7,7 @@ import formatReservationTime from "./format-reservation-date";
 
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+  process.env.REACT_APP_API_BASE_URL || "https://periodic-tables-backend-lhvm.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
